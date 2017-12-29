28b08efed4b19b71fe239ce4f329c20489743388
bobby569 <zhan2175@purdue.edu>
2017-12-29 11:48:14 -0500

    format

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Vue.js-Example
+## Vue.js Example
 
-Vue.js Example Code
-
-Credit to Danny Markov, 2016
-
-[link](http://tutorialzine.com/2016/03/5-practical-examples-for-learning-vue-js/)
+Some simple Vue.js example. Credit to [Danny Markov](http://tutorialzine.com/2016/03/5-practical-examples-for-learning-vue-js/)
--- example2/script.js
+++ example2/script.js
@@ -1,23 +1,20 @@
 // Creating a new Vue instance and pass in an options object.
 var demo = new Vue({
-
-    // A DOM element to mount our view model.
-    el: '#main',
-
-    // Define properties and give them initial values.
-    data: {
-        show_tooltip: false,
-        text_content: 'Edit me.'
-    },
-
-    // Functions we will be using.
-    methods: {
-        hideTooltip: function(){
-            // When a model is changed, the view will be automatically updated.
-            this.show_tooltip = false;
-        },
-        toggleTooltip: function(){
-            this.show_tooltip = !this.show_tooltip;
-        }
-    }
-})
+	// A DOM element to mount our view model.
+	el: '#main',
+	// Define properties and give them initial values.
+	data: {
+		show_tooltip: false,
+		text_content: 'Click and Edit me.'
+	},
+	// Functions we will be using.
+	methods: {
+		hideTooltip() {
+			// When a model is changed, the view will be automatically updated.
+			this.show_tooltip = false;
+		},
+		toggleTooltip() {
+			this.show_tooltip = !this.show_tooltip;
+		}
+	}
+});
